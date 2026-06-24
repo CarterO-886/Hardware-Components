dacb498e28d0af4e0258968c8d6d7a24af19a03b
Carter Oates <carter.oates833@gmail.com>
2026-06-24 11:27:37 -0700

    added comments

--- clk_div_template.sv
+++ clk_div_template.sv
@@ -1,20 +1,20 @@
 `timescale 1ns/1ps
 
 module clk_div(
-    int parameter DIVISOR = 4
+    int parameter DIVISOR = 4 //How many input clock cycles to divide by
 )(
     input logic rst,
     input logic clk,
     output logic sclk
 
 );
-
+    //Count signal width fitting
     logic WIDTH = $clog2(DIVISOR);
     logic [WIDTH-1:0]count;
 
     initial count = '0;
     initial sclk = 1'b0;
-
+    
     while(count <(DIVISOR/2)) //Full clock cycle is half high and half low
         if (count == DIVISOR)
             sclk <= ~sclk;
